afc081acb2359e7c82592ca57ab98c32fe8de4e1
KauaCampos <kauauwc@gmail.com>
2024-11-16 00:59:56 -0300

    lista 15 problema 2292

--- llp1/src/main/java/br/cefetmg/inf/llp/lista15/p2292/Main.java
+++ llp1/src/main/java/br/cefetmg/inf/llp/lista15/p2292/Main.java
@@ -3,34 +3,33 @@ package br.cefetmg.inf.llp.lista15.p2292;
 import java.util.Scanner;
 
 public class Main {
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.nextLine();
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine(); 
 
-        for (int i = 0; i < n; i++) {
-            String[] input = scanner.nextLine().split(" ");
-            String initialConfig = input[0];
-            long c = Long.parseLong(input[1]);
+        while (n-- > 0) {
+            String[] entrada = sc.nextLine().split(" ");
+            String painelInicial = entrada[0];
+            long c = Long.parseLong(entrada[1]);
 
-            StringBuilder painel = new StringBuilder(initialConfig);
+            char[] painel = painelInicial.toCharArray();
+            int tamanho = painel.length;
 
-            boolean primeiraLampadaLigada = painel.charAt(0) == 'O';
-            if (c % 2 != 0) {
-                primeiraLampadaLigada = !primeiraLampadaLigada;
-            }
-            painel.setCharAt(0, primeiraLampadaLigada ? 'O' : 'X');
-
-            for (int j = 1; j < painel.length(); j++) {
-                if (painel.charAt(j - 1) == 'X') {
-                    painel.setCharAt(j, painel.charAt(j) == 'O' ? 'X' : 'O');
+            for (int i = 0; i < tamanho; i++) {
+                if (c % 2 == 1) { 
+                    painel[i] = painel[i] == 'O' ? 'X' : 'O';
                 }
+
+                c /= 2;
+                if (c == 0) break; 
             }
 
-            System.out.println(painel.toString());
+            System.out.println(new String(painel));
         }
 
-        scanner.close();
+        sc.close();
     }
 }
-
